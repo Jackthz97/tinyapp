8f9d7d063aed90980b061153e7537b5e4b43b0c2
Tian Hao Zhang <97329471+Jackthz97@users.noreply.github.com>
2022-02-21 15:49:10 -0500

    Add a route '/hello' that displays Hello World using HTML code

--- express_server.js
+++ express_server.js
@@ -2,6 +2,10 @@ const express  = require("express");
 const app = express();
 const PORT = 8080; // default port
 
+app.listen(PORT, () => {
+  console.log(`Example app listening on port: ${PORT}!`);
+});
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -16,6 +20,6 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port: ${PORT}!`);
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
