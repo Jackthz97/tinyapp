eda2fc6dc98a5d33b976a335fe70057d855c43d8
Tian Hao Zhang <97329471+Jackthz97@users.noreply.github.com>
2022-02-22 15:21:53 -0500

    Added POST route that removes a URL and redirect the client back to the '/urls' page

--- express_server.js
+++ express_server.js
@@ -4,6 +4,7 @@ const app = express();
 const PORT = 8080; // default port
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 app.listen(PORT, () => {
@@ -34,6 +35,11 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortURL}`);
 });
 
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls");
+});
+
 // Route to the forms page
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
@@ -47,7 +53,7 @@ app.get("/urls/:shortURL", (req, res) => {
 
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
-  res.redirect(`http://${longURL}`);
+  res.redirect(`${longURL}`);
 });
 
 app.get("/urls.json", (req, res) => {
