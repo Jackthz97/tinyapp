45a3f2319a68ec2057c01dd86f4a1ded20caf70b
Tian Hao Zhang <97329471+Jackthz97@users.noreply.github.com>
2022-02-21 23:10:16 -0500

    Added a new route that redirect any request to /u/:shortURL to its longURL

--- express_server.js
+++ express_server.js
@@ -45,6 +45,11 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(`http://${longURL}`);
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
