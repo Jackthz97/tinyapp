165599c85aedd7f6201f0efa125a79f07290d2d2
Tian Hao Zhang <97329471+Jackthz97@users.noreply.github.com>
2022-02-25 13:57:57 -0500

    Exports all the helper functions

---
+++ helperFunctions.js
@@ -0,0 +1,28 @@
+ 
+const checkEmail = function(email, userData) {
+  for (let userId in userData) {
+    if (email === userData[userId].email) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const checkUserId = function(email, userData)  {
+  for (let id in userData) {
+    if (email === userData[id].email) {
+      return id;
+    }
+  }
+};
+
+// Random string generator
+const generateRandomString = () => {
+  const string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+  const arr = [];
+  for (let i = 0; i < 6; i++) {
+    arr.push(string[Math.floor(Math.random() * 62)]);
+  }
+  return arr.join("");
+};
+module.exports = {checkEmail, checkUserId, generateRandomString};
